76b9b2a35f8d3fc9ba37eaf8038d86ba77834c9a
tumark0305 <162888496+tumark0305@users.noreply.github.com>
2024-12-02 08:35:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import os,requests
+class jason:
+    class define:
+        def __init__(self,_question):
+            self.question = _question
+            self.answer = None
+            self.error = False
+            return None
+
 
